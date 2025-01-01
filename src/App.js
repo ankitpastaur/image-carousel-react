f3684fb8746a0aa995de39d25bb194a2cf77d1c6
ankitpastor <pastorankit18@gmail.com>
2025-01-01 12:59:40 -0500

    Added basic structure for image-carousel app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button>{"<"}</button>
+      <img src="" alt="Image-not-found"></img>
+      <button>{">"}</button>
+    </div>
+  );
+}
+
+export default App;
